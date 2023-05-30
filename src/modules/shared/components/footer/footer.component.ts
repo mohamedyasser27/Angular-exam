13ba459e4ca1961c5afc90660ce047ff22b4c4c4
mohamedyasser27 <my2722000@gmail.com>
2023-05-30 13:34:04 +0300

    Add The <Footer/> and handle all the responsivity

--- src/modules/shared/components/footer/footer.component.ts
+++ src/modules/shared/components/footer/footer.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent {
+  constructor(private fb: FormBuilder) {}
 
+  getAppForm = this.fb.nonNullable.group({
+    email: ['', Validators.required],
+  });
+  onSubmit() {
+    console.log(this.getAppForm.value);
+  }
 }
