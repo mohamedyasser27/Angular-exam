bfe710802fb9f1a3157b8be07c0757d7245c6793
mohamedyasser27 <my2722000@gmail.com>
2023-05-30 23:46:20 +0300

    Add Basic Routing
    i still need to work on Auth to guard paths and work
    on failure Path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginFormComponent } from 'src/modules/auth/components/login-form/login-form.component';
+import { SignUpFormComponent } from 'src/modules/auth/components/sign-up-form/sign-up-form.component';
+import { ProductListComponent } from 'src/modules/products/components/product-list/product-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: ProductListComponent },
+  { path: 'login', component: LoginFormComponent },
+  { path: 'signUp', component: SignUpFormComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  // { path: '**', component: ErrorComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
