ef3cf560b81f16c6a0028c0e1514a93d50606af5
mohamedyasser27 <my2722000@gmail.com>
2023-05-31 10:56:41 +0300

    Finish the <ProductDetails/> component

--- src/modules/products/components/product-details/product-details.component.ts
+++ src/modules/products/components/product-details/product-details.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
+import { ProductApiService } from '../../services/product-api.service';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
-  styleUrls: ['./product-details.component.scss']
+  styleUrls: ['./product-details.component.scss'],
 })
 export class ProductDetailsComponent {
-
 }
