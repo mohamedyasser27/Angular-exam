16fdf9b8d15d28e3c9430ffbaaac8c2b15776c41
mohamedyasser27 <my2722000@gmail.com>
2023-05-31 19:06:09 +0300

    Add Star Rating to product

--- src/modules/products/components/product/product.component.ts
+++ src/modules/products/components/product/product.component.ts
@@ -4,16 +4,24 @@ import { MatCardModule } from '@angular/material/card';
 import { CurrencyPipe } from '@angular/common';
 import { Product } from '../../types/Product';
 import { RouterLink } from '@angular/router';
+import { StarRatingModule } from 'angular-star-rating';
+
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.scss'],
   standalone: true,
-  imports: [MatCardModule, MatButtonModule, CurrencyPipe, RouterLink],
+  imports: [
+    MatCardModule,
+    MatButtonModule,
+    CurrencyPipe,
+    RouterLink,
+    StarRatingModule
+  ],
 })
 export class ProductComponent {
   @Input() inputProduct: Product = {
-    id:0,
+    id: 0,
     title: '',
     price: 0,
     description: '',
